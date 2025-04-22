2659ab5eddc48f98ca8c3867d4141a036c9332b1
karai-X <karai-X@users.noreply.github.com>
2025-04-22 23:03:08 +0900

    first

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+  _value = 0;
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other) {
+  std::cout << "Copy constructor called" << std::endl;
+  _value = other.getRawBits();
+}
+
+Fixed::~Fixed(void) { std::cout << "Destructor called" << std::endl; }
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &other) {
+    this->_value = other.getRawBits();
+  }
+  return *this;
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return _value; }
+
+void Fixed::setRawBits(int const raw) {
+
+  std::cout << "setRawBits member function called" << std::endl;
+  _value = raw; }
---
+++ ex01/Fixed.cpp
@@ -0,0 +1,49 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+  _value = 0;
+  std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) {
+  std::cout << "Int constructor called" << std::endl;
+  _value = value << 8;
+}
+
+Fixed::Fixed(const float value) {
+  std::cout << "Float constructor called" << std::endl;
+
+  _value = roundf(value * (1 << _fractional_bits));
+}
+
+Fixed::Fixed(const Fixed &other) {
+  std::cout << "Copy constructor called" << std::endl;
+  _value = other.getRawBits();
+}
+
+Fixed::~Fixed(void) { std::cout << "Destructor called" << std::endl; }
+
+Fixed &Fixed::operator=(const Fixed &other) {
+  std::cout << "Copy assignment operator called" << std::endl;
+  if (this != &other) {
+    this->_value = other.getRawBits();
+  }
+  return *this;
+}
+
+int Fixed::getRawBits(void) const {
+  std::cout << "getRawBits member function called" << std::endl;
+  return _value;
+}
+
+void Fixed::setRawBits(int const raw) {
+
+  std::cout << "setRawBits member function called" << std::endl;
+  _value = raw;
+}
+
+float Fixed::toFloat(void) const {
+  return (static_cast<float>(_value)) / (1 << _fractional_bits);
+}
+
+int Fixed::toInt(void) const { return _value >> _fractional_bits; }
